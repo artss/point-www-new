16eef1231268ed95f42f2139cc06387183ae7880
Artem Sazhin <arts@psto.net>
2015-05-05 08:46:21 +0300

    login-form.js -> landing.js

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,2 +1,20 @@
-define(["login-form"], function () {
+define(["jquery"], function ($) {
+  var wrap = $(".login-wrap");
+
+  wrap.find(".reg-link").click(function (evt) {
+    evt.preventDefault();
+    wrap.removeClass("login");
+  });
+
+  wrap.find(".login-link").click(function (evt) {
+    evt.preventDefault();
+    wrap.addClass("login");
+  });
+
+  var recaptcha = wrap.find(".recaptcha");
+
+  wrap.find(".reg-form input").focus(function () {
+    if (recaptcha.hasClass("open")) return;
+    recaptcha.addClass("open");
+  });
 });
