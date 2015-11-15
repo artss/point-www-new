4227c0863b48493c694c70a022b6d298f215753b
Artem Sazhin <arts@psto.net>
2015-11-15 15:21:01 +0300

    files counter

--- static/js/post-form.js
+++ static/js/post-form.js
@@ -21,11 +21,27 @@ define(function(require) {
 
   var PostFormView = Form.View.extend({
     events: _.extend({}, Form.View.prototype.events, {
-      'click .btn-cancel': 'cancel'
+      'click .btn-cancel': 'cancel',
+      'change .js-file': 'updateUploads'
     }),
 
     cancel: function() {
       Backbone.trigger('new-post-cancel');
+    },
+
+    updateUploads: function (evt) {
+      var upload = this.$('.upload')[0];
+      var number = this.$('.upload .number')[0];
+
+      var files = evt.target.files;
+
+      if (files.length > 0) {
+        upload.classList.add('positive');
+      } else {
+        upload.classList.remove();
+      }
+
+      number.innerHTML = files.length;
     }
   });
 
